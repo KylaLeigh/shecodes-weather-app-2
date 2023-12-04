8453738a819763ec2b4bf23d504e75ad5e052533
KylaLeigh <kyla.llee@gmail.com>
2023-12-04 11:26:17 +0000

    Inject additional weather data
    Added; date, time, humidity, wind speed, description.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let currentTemperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
 
+  console.log(1);
+  console.log(response.data.condition.description);
+
+  dateElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = response.data.wind.speed;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 0) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchForCity(city) {
